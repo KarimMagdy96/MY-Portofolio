d648b7ab65e26a4919616d0a1ae42eba60406bbc
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-18 01:56:29 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import './App.scss'
+
+function App() {
+ 
+
+  return (
+    <>
+<Routes>
+  <Route path='/' element={<Layout/>} />
+</Routes>
+    </>
+  )
+}
+
+export default App
