52214f336712eceb47623c5a788cb679cdff0cdb
Karim Magdy Mohamed <49499557+KarimMagdy96@users.noreply.github.com>
2022-11-18 03:18:21 +0200

    update

--- src/Componants/Sidebar/Sidebar.jsx
+++ src/Componants/Sidebar/Sidebar.jsx
@@ -7,12 +7,18 @@ export default function Sidebar() {
   return (
     <div className="nav-bar">
       <Link className="logo" to="#">
-        <img className='logo-img' src='../../../public/logo.png' alt="Karim" />
+        <img className="logo-img" src="../../../public/logo.png" alt="Karim" />
       </Link>
       <nav>
-      <NavLink exact='true' activeclassname='active' to='/'>
-        
-      </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <i class="fa-solid fa-house"></i>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <i class="fa-solid fa-house"></i>
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <i class="fa-solid fa-house"></i>
+        </NavLink>
       </nav>
     </div>
   ); 
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
